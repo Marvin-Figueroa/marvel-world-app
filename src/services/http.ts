e155b7c1da17a1c54a5f9bcbd89c622234eb6c17
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-12-20 22:15:52 -0600

    Remove unused code

--- src/services/http.ts
+++ src/services/http.ts
@@ -11,7 +11,6 @@ instance.interceptors.response.use(null, (error) => {
     error.response.status < 500;
 
   if (!expectedError) {
-    console.log('An error ocurred while getting the data.', error);
     alert('An unexpected error ocurred while getting the data.');
   }
 
@@ -24,22 +23,3 @@ export default {
   put: instance.put,
   delete: instance.delete,
 };
-// async function http<T>(path: string, config: RequestInit): Promise<T> {
-//   const request = new Request(path, config);
-//   const response = await fetch(request);
-
-//   if (!response.ok) {
-//     throw new Error('Could not get the data. ' + response.statusText);
-//   }
-
-//   return response.json().catch((error) => {
-//     throw new Error(
-//       'An error ocurred while trying to get the response. ' + error.toString()
-//     );
-//   });
-// }
-
-// export async function get<T>(path: string, config?: RequestInit): Promise<T> {
-//   const init = { method: 'get', ...config };
-//   return http<T>(path, init);
-// }
