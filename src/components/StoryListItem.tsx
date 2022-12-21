156ed113e93535664faf36d04bb2b330cfaf116f
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-12-20 22:35:50 -0600

    Add bookmarking functionality to the StoryListItem component

--- src/components/StoryListItem.tsx
+++ src/components/StoryListItem.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { IStory } from '../models/story';
+import LikeButton from './LikeButton';
+import { useDispatch, useSelector } from 'react-redux';
+import * as actions from '../store/storiesSlice';
+import { RootState } from '../store/store';
 import './StoryListItem.scss';
+import { useState } from 'react';
 
 type Props = {
   story: IStory;
 };
 
 function StoryListItem({ story }: Props) {
+  const favStories = useSelector(
+    (state: RootState) => state.stories.favoriteStories
+  );
+  const [isFavStory, setIsFavStory] = useState(
+    () => favStories.findIndex((favStorie) => favStorie.id === story.id) !== -1
+  );
+  const dispatch = useDispatch();
+
+  function handleClick() {
+    setIsFavStory((prevIsFavStory) => !prevIsFavStory);
+
+    if (isFavStory) {
+      dispatch(actions.storyUnBookmarked(story.id));
+    } else {
+      dispatch(actions.storyBookmarked(story));
+    }
+  }
+
   return (
     <article className='story-item'>
       <Link to={`/stories/${story.id}`} className='story-item__title'>
         {story.title || 'No title available'}
       </Link>
+      <LikeButton liked={isFavStory} onToggle={handleClick} />
     </article>
   );
 }
