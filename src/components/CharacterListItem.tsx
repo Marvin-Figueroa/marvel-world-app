f81c2807dc1fe8577ac0f07754c8edfb9baed25f
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-12-20 22:20:41 -0600

    Add bookmarking functionality to CharacterListItem

--- src/components/CharacterListItem.tsx
+++ src/components/CharacterListItem.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ICharacter } from '../models/character';
 import './CharacterListItem.scss';
+import LikeButton from './LikeButton';
+import { useDispatch, useSelector } from 'react-redux';
+import * as actions from '../store/charactersSlice';
+import { RootState } from '../store/store';
 
 type Props = {
   character: ICharacter;
 };
 
 function CharacterListItem({ character }: Props) {
+  const favCharacters = useSelector(
+    (state: RootState) => state.characters.favoriteCharacters
+  );
+  const [isFavCharacter, setIsFavCharacter] = useState(
+    () =>
+      favCharacters.findIndex((favChar) => favChar.id === character.id) !== -1
+  );
+  const dispatch = useDispatch();
+
+  function handleClick() {
+    setIsFavCharacter((prevIsFavCharacter) => !prevIsFavCharacter);
+
+    if (isFavCharacter) {
+      dispatch(actions.characterUnBookmarked(character.id));
+    } else {
+      dispatch(actions.characterBookmarked(character));
+    }
+  }
+
   return (
     <article className='character-item'>
       <img
@@ -19,9 +42,12 @@ function CharacterListItem({ character }: Props) {
         }
         alt={character.name}
       />
-      <p className='character-item__name'>
-        <Link to={`/characters/${character.id}`}>{character.name}</Link>
-      </p>
+      <div className='character-item__footer'>
+        <p className='character-item__name'>
+          <Link to={`/characters/${character.id}`}>{character.name}</Link>
+        </p>
+        <LikeButton liked={isFavCharacter} onToggle={handleClick} />
+      </div>
     </article>
   );
 }
