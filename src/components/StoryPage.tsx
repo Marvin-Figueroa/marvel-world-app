6fdf59d02538c00f8bde4b3f5304d33aa9d419ac
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-12-20 22:36:58 -0600

    Add filtering functionality to the StoryPage component

--- src/components/StoryPage.tsx
+++ src/components/StoryPage.tsx
@@ -4,15 +4,25 @@ import { HashLoader } from 'react-spinners';
 import { getPaginatedStories } from '../services/stories';
 import { RootState } from '../store/store';
 import * as actions from '../store/storiesSlice';
+import FilterSelect from './FilterSelect';
 import Pagination from './Pagination';
 import StoryList from './StoryList';
+import './StoryPage.scss';
+
+const charactersWithStories = [
+  '1011334',
+  '1017100',
+  '1009144',
+  '1010699',
+  '1009146',
+];
 
 function StoryPage() {
   const [loadingStories, setLoadingStories] = useState(true);
   const [totalItems, setTotalItems] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
   const dispatch = useDispatch();
-  const stories = useSelector((state: RootState) => state.stories);
+  const stories = useSelector((state: RootState) => state.stories.stories);
+  const filters = useSelector((state: RootState) => state.stories.filters);
 
   useEffect(() => {
     getPaginatedStories().then((data) => {
@@ -24,23 +34,44 @@ function StoryPage() {
     });
   }, []);
 
-  useEffect(() => {
+  function handlePageChange(pageNumber: number) {
+    dispatch(actions.storiesFilteredByPage(pageNumber));
     setLoadingStories(true);
-    getPaginatedStories(currentPage, 10).then((data) => {
+
+    getPaginatedStories(pageNumber, 10, filters.character).then((data) => {
       if (data) {
         dispatch(actions.storiesLoaded(data.results));
         setTotalItems(data.total);
       }
       setLoadingStories(false);
     });
-  }, [currentPage]);
+  }
 
-  function handlePageChange(pageNumber: number) {
-    setCurrentPage(pageNumber);
+  function handleCharacterSelectFilterChange(character: string) {
+    const validatedCharacter =
+      character === 'Select an option' ? '' : character;
+
+    dispatch(actions.storiesFilteredByCharacter(validatedCharacter));
+
+    setLoadingStories(true);
+
+    getPaginatedStories(1, 10, validatedCharacter).then((data) => {
+      if (data) {
+        dispatch(actions.storiesLoaded(data.results));
+        setTotalItems(data.total);
+      }
+      setLoadingStories(false);
+    });
   }
 
   return (
-    <>
+    <main className='story-page'>
+      <FilterSelect
+        label='character'
+        value={filters.character}
+        options={charactersWithStories}
+        onFilterChange={handleCharacterSelectFilterChange}
+      />
       {loadingStories ? (
         <HashLoader color='#dc143c' />
       ) : (
@@ -52,9 +83,9 @@ function StoryPage() {
         pageSize={10}
         onPageChange={handlePageChange}
         siblingCount={1}
-        currentPage={currentPage}
+        currentPage={filters.page || 1}
       />
-    </>
+    </main>
   );
 }
 
