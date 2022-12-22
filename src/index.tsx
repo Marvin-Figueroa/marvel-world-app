57cae6da20c32342a5ff0d6cbfc9577fa3d36daf
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-12-22 17:47:13 -0600

    Add redux-persist to persist bookmarks after reloading the page or opening it on a new tab.

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './store/store';
+import { store, persistor } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { HashLoader } from 'react-spinners';
+
 import './index.scss';
 
 const root = ReactDOM.createRoot(
@@ -11,7 +14,11 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate
+        loading={<HashLoader color='#dc143c' />}
+        persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
